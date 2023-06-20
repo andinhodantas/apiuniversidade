6305bd9d45f1a3bdd69f5c0f9b67632d448518a8
andinhodantas <andersonnascimentodantas@gmail.com>
2023-06-20 14:09:36 -0300

    Banco para Azure

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 //Add DbContext to acess PostgreSQL.
-var connectionString = builder.Configuration.GetConnectionString("PostgerSQL");
+var connectionString = builder.Configuration.GetConnectionString("Azure");
 builder.Services.AddDbContext<ApiUniversidadeContext>(options=> options.UseNpgsql(connectionString));
 
 // Add services to the container.
